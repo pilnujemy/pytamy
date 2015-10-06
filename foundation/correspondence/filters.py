0a2fcbaf70614645d3318e0d7a157a28cd99ae1d
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-06 06:59:55 +0200

    Added django-atom v0.5.0, optimize correspondence.filters

--- foundation/correspondence/filters.py
+++ foundation/correspondence/filters.py
@@ -1,53 +1,19 @@
-import django_filters
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout
-import autocomplete_light
+from django_filters import FilterSet, DateRangeFilter
 from .models import Letter, Contact
+from atom.ext.django_filters import AutocompleteChoiceFilter, CrispyFilterMixin
 
 
-class ContactFilter(django_filters.FilterSet):
-    created_on = django_filters.DateRangeFilter()
-
-    @property
-    def form(self):
-        self._form = super(ContactFilter, self).form
-        self._form.helper = FormHelper()
-        self._form.helper.form_class = 'form-inline'
-        self._form.helper.form_method = 'get'
-        self._form.helper.layout = Layout(
-            'postcode',
-            'city',
-            'created_by',
-            'created_on',
-            Submit('save', 'save')
-        )
-        return self._form
+class ContactFilter(CrispyFilterMixin, FilterSet):
+    created_on = DateRangeFilter()
 
     class Meta:
         model = Contact
         fields = ['postcode', 'city', 'created_by', 'created_on']
 
 
-class LetterFilter(django_filters.FilterSet):
-    created_on = django_filters.DateRangeFilter()
-    contact = django_filters.ModelChoiceFilter(
-        queryset=Contact.objects.all(),
-        widget=autocomplete_light.ChoiceWidget('ContactAutocomplete')
-        )
-
-    @property
-    def form(self):
-        self._form = super(LetterFilter, self).form
-        self._form.helper = FormHelper()
-        self._form.helper.form_class = 'form-inline'
-        self._form.helper.form_method = 'get'
-        self._form.helper.layout = Layout(
-            'outgoing',
-            'contact',
-            'created_on',
-            Submit('save', 'save')
-        )
-        return self._form
+class LetterFilter(CrispyFilterMixin, FilterSet):
+    created_on = DateRangeFilter()
+    contact = AutocompleteChoiceFilter('ContactAutocomplete')
 
     class Meta:
         model = Letter
