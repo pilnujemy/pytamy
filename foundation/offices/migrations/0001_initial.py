2dcf9c3740b11f0a02f520c660aaf2c2116d02c0
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-18 15:59:26 +0200

    Rebuild migrations for offices&teryt

--- foundation/offices/migrations/0001_initial.py
+++ foundation/offices/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 import django.utils.timezone
 from django.conf import settings
 import model_utils.fields
@@ -11,8 +11,8 @@ import django_states.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('teryt', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -21,8 +21,12 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('email', models.EmailField(max_length=254, verbose_name='Address')),
-                ('default', models.BooleanField(default=True)),
+                ('default', models.BooleanField(default=True, help_text='Use this e-mail as primary for office', verbose_name='Default')),
             ],
+            options={
+                'verbose_name': 'Adres e-mail',
+                'verbose_name_plural': 'E-mail addresses',
+            },
         ),
         migrations.CreateModel(
             name='Office',
@@ -31,13 +35,15 @@ class Migration(migrations.Migration):
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
                 ('name', models.CharField(max_length=150, verbose_name='Nazwa')),
+                ('verified', models.BooleanField(default=False, verbose_name='Zweryfikowany')),
                 ('state', django_states.fields.StateField(default=b'created', max_length=100)),
                 ('created_by', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
                 ('jst', models.ForeignKey(to='teryt.JST')),
-                ('parent', models.ManyToManyField(related_name='parent_rel_+', to='offices.Office')),
+                ('parent', models.ManyToManyField(related_name='_parent_+', to='offices.Office')),
             ],
             options={
-                'abstract': False,
+                'verbose_name': 'Office',
+                'verbose_name_plural': 'Offices',
             },
         ),
         migrations.CreateModel(
@@ -45,15 +51,20 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('state', django_states.fields.StateField(default=b'transition_initiated', max_length=100, verbose_name='state id')),
-                ('from_state', models.CharField(max_length=100, choices=[(b'removed', 'Removed'), (b'accepted', 'Accepted'), (b'created', 'Office created')])),
-                ('to_state', models.CharField(max_length=100, choices=[(b'removed', 'Removed'), (b'accepted', 'Accepted'), (b'created', 'Office created')])),
+                ('from_state', models.CharField(max_length=100, choices=[(b'destroyed', 'Destroyed'), (b'accepted', 'Accepted'), (b'created', 'Office created')])),
+                ('to_state', models.CharField(max_length=100, choices=[(b'destroyed', 'Destroyed'), (b'accepted', 'Accepted'), (b'created', 'Office created')])),
                 ('serialized_kwargs', models.TextField(blank=True)),
                 ('start_time', models.DateTimeField(auto_now_add=True, verbose_name='transition started at', db_index=True)),
                 ('on', models.ForeignKey(related_name='state_history', to='offices.Office')),
                 ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
-                'verbose_name': 'office transition',
+                'verbose_name': 'Office transition',
             },
+        ),
+        migrations.AddField(
+            model_name='email',
+            name='office',
+            field=models.ForeignKey(verbose_name='Office', to='offices.Office'),
         ),
     ]
