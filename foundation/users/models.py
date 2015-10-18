9f9b626bddcd10188afc995775e32bd60192e563
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-18 14:26:56 +0200

    Add tests for foundation.users

--- foundation/users/models.py
+++ foundation/users/models.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 # from django.utils.translation import ugettext_lazy as _
 
 
+@python_2_unicode_compatible
 class User(AbstractUser):
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
+
+    def get_absolute_url(self):
+        return reverse('users:detail', kwargs={'username': self.username})
