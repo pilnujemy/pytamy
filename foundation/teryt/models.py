e0fab1ddc542cf44bd3612eece68a0f309d1a523
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-19 03:34:44 +0200

    Added offices in region

--- foundation/teryt/models.py
+++ foundation/teryt/models.py
@@ -1,8 +1,14 @@
 from teryt_tree.models import JednostkaAdministracyjna
 from django.core.urlresolvers import reverse
+from cached_property import cached_property
 
 
 class JST(JednostkaAdministracyjna):
+
+    @cached_property
+    def get_offices_in_region(self):
+        Office = self.office_set.model
+        return Office.objects.area(self)
 
     def get_absolute_url(self):
         if self.level == 0:
