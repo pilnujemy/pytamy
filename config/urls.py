ab02e23bd8a41f2bc3a2bf733baec47e303f0fce
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-11-29 01:55:34 +0100

    Added django.contrib.flatpages

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,9 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', include('foundation.main.urls')),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Django Admin
     url(r'^admin/', include(admin.site.urls)),
