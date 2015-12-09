b49229f8d2d2e5805cb3b11b5d36a7e40228b0b1
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-12-09 02:00:33 +0100

    Filter cases by office__tags

--- foundation/cases/filters.py
+++ foundation/cases/filters.py
@@ -2,9 +2,12 @@
 from atom.filters import CrispyFilterMixin
 import django_filters
 from .models import Case
+from taggit.models import Tag
 
 
 class CaseFilter(CrispyFilterMixin, django_filters.FilterSet):
+    office__tags = django_filters.ModelChoiceFilter(queryset=Tag.objects.all())
+
     def __init__(self, *args, **kwargs):
         super(CaseFilter, self).__init__(*args, **kwargs)
 
