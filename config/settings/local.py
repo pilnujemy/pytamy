850a3736ba792ff7384df2e950421f6da44e856e
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-12-14 14:09:34 +0100

    Drop django-debug-toolbar & django-autofixture

--- config/settings/local.py
+++ config/settings/local.py
@@ -40,7 +40,9 @@ CACHES = {
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
+"""
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
 INSTALLED_APPS += ('debug_toolbar', 'autofixture',)
 
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
@@ -51,7 +53,7 @@ DEBUG_TOOLBAR_CONFIG = {
     ],
     'SHOW_TEMPLATE_CONTEXT': True,
 }
-
+"""
 # django-extensions
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ('django_extensions', )
