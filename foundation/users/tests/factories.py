b46d5a38f0e25d70a19e53c05abd1d4234fb95e1
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-12-17 17:09:53 +0100

    Set password in UserFactory

--- foundation/users/tests/factories.py
+++ foundation/users/tests/factories.py
@@ -6,6 +6,8 @@ import factory
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence('user-{0}'.format)
     email = factory.Sequence('user-{0}@example.com'.format)
+    password = factory.PostGenerationMethodCall('set_password', 'password')
 
     class Meta:
         model = models.User
+        django_get_or_create = ('username', )
